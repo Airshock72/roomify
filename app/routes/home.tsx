abc205ab45aab6c05c058e80c7a42e7d5da0d9af
glomidze <glomidze@ita.gov.ge>
2026-03-13 15:00:48 +0400

    Remove initial welcome page and add ESLint

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,10 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
-
-export function meta({}: Route.MetaArgs) {
+export function meta() {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+    { title: 'New React Router App' },
+    { name: 'description', content: 'Welcome to React Router!' }
+  ]
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <h1 className='text-3xl text-indigo-700 font-extrabold'>Home</h1>
 }
